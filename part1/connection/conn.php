2512998b82651d67747308c0504a7aebbf87bb01
gouyoun <69454249+gouyoun@users.noreply.github.com>
2021-10-19 20:37:29 +0300

    Update conn.php

--- part1/connection/conn.php
+++ part1/connection/conn.php
@@ -1,5 +1,6 @@
 <?php
 //connecting to the database
+//Test
 $con = mysqli_connect("localhost","root","","north21");
 //if the connection is unsuccessful, returns a message and the error.
 if(!$con){
